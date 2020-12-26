91b21e70a170f3164d09b75e67bfb6eeeb92bffc
13alvone <13alvone@gmail.com>
2020-12-25 22:57:47 -0600

    ** Major refactoring of everything as well as structure adjustments. ** Included the awesome function from https://github.com/thavelick/summarize/ to attempt to summarize url data/html data that is returned.

--- gmailrunner.py
+++ gmailrunner.py
@@ -1,128 +1,88 @@
-from __future__ import print_function
-from googleapiclient.discovery import build
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
+import summarize
+from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import metadata_parser
+import email.header
 import requests
-import pickle
-import os.path
-import base64
+import datetime
+import argparse
+import imaplib
+import logging
+import email
+import math
+import time
+import nltk
+import sys
+import os
 import re
 
-# Global variables
-scopes = ['https://www.googleapis.com/auth/gmail.readonly']
-_path = '/root/gmailrunner.tags'
-_trash_set = set()
+# Global Variables
+gmail_email = os.environ.get('GMAIL_EMAIL')
+gmail_passwd = os.environ.get('GMAIL_PASSWD')
+gmail_folder = "INBOX"
+start_time = time.time()
+gmail_tags_path = 'gmailrunner.tags'
 master_tags = set()
 master_dict = {}
+url_blacklist = [                                               # This is a `contains` blacklist, non-explicit
+    'https://myaccount.google.com/notifications',
+    'https://accounts.google.com/AccountChooser',
+]
+sender_whitelist = [
+    'email_address_@_to_ignore.com',
+    'other_address_@_to_ignore.com',
+    'etc@you_get_it.com',
+]
+simple_summarizer = summarize.SimpleSummarizer()
 
 
-def authorize():
-    global scopes
-    creds = None
-    if os.path.exists('token.pickle'):
-        with open('token.pickle', 'rb') as token:
-            creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', scopes)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.pickle', 'wb') as token:
-            pickle.dump(creds, token)
-    gmail_service = build('gmail', 'v1', credentials=creds)
-    return gmail_service
+def build_url_obj(_url):
+    global master_tags
+    meta_strategy = ['og', 'dc', 'meta', 'page']
+    tags, page = [], None
+    url_obj = {
+        'title': '',
+        'description': '',
+        'image': set(),
+        'site_name': '',
+        'item_tags': {},
+    }
 
+    try:
+        url_obj['description'] = summarize_url_content(_url)
+    except Exception as e:
+        logging.info(f'[!] The following url failed NTLK summarization:\n{_url}\n')
 
-def gather_urls():
-    global _trash_set
-    raw_url_list = []
-    url_list = set()
-    service = authorize()
-    results = service.users().messages().list(userId='me').execute()
-    email_messages = results['messages']
-    for email_message in email_messages:
-        thread_id = email_message['id']
-        _trash_set.add(thread_id)
-        message_container = service.users().messages().get(userId='me', id=email_message['id']).execute()
-        message_subcontainer = message_container['payload']['parts']
-        for item in message_subcontainer:
-            try:
-                base64_message = item['body']['data']
-                message_bytes = base64.b64decode(base64_message)
-                message = message_bytes.decode('ascii')
-                print(f'Item: {item}\nMessage: {message}\n\n')
-                raw_url_list.append(message)
-            except:
-                pass
-    for raw_url in raw_url_list:
-        if 'http' in raw_url:
-            unclean_list = re.findall(r'(https?://\S+)', raw_url)
-            for unclean_item in unclean_list:
-                if unclean_item not in url_list:
-                    url_list.add(unclean_item)
-    return url_list
+    try:
+        page = metadata_parser.MetadataParser(url=_url, search_head_only=False)
+        if page and url_obj['title'] != '':
+            url_obj['title'] = page.get_metadatas('title', strategy=meta_strategy)
+        if page and url_obj['description'] != '':
+            url_obj['description'] = page.get_metadatas('description', strategy=meta_strategy)
+        if page and url_obj['site_name'] != '':
+            url_obj['site_name'] = page.get_metadatas('site_name', strategy=meta_strategy)
+        if page and url_obj['image']:
+            url_obj['image'].add(page.get_metadata_link('image'))
+    except Exception:
+        pass
 
-
-def populate_master_tags():
-    global _path, master_tags
-    f_in = open(_path, 'r')
-    for line in f_in:
-        x = line.replace('\r\n', '').replace('\r', '').replace('\n', '')
-        y = x.lstrip(' ').rstrip(' ').replace(' ', '_')
-        master_tags.add(y.lower())
-
-
-def build_url_obj(url):
-    global master_dict, master_tags
-    response = requests.get(url)
+    response = requests.get(_url)
     soup = BeautifulSoup(response.text, 'lxml')
     metas = soup.find_all('meta')
-    tags = []
-    preview_obj = {
-        'og_title': '',
-        'og_description': '',
-        'og_image': '',
-        'og_site_name': ''
-    }
-    url_obj = {
-        'full_title': '',
-        'short_title': '',
-        'site_name': '',
-        'image_urls': [],
-        'item_tags': {},
-        'item_type': '',
-        'preview_obj': {},
-        'description': '',
-    }
 
     for meta in metas:
-        if 'name' in meta.attrs and meta.attrs['name'] == 'title':
-            url_obj['full_title'] = meta.attrs['content']
-            tags.append(meta.attrs['content'])
         if 'property' in meta.attrs:
-            if meta.attrs['property'] == 'og:title':
-                url_obj['short_title'] = meta.attrs['content']
-                preview_obj['og_title'] = meta.attrs['content']
+            if 'title' in meta.attrs['property']:
+                url_obj['title'] = meta.attrs['content']
                 tags.append(meta.attrs['content'])
-            if meta.attrs['property'] == 'og:image':
-                url_obj['image_urls'].append(meta.attrs['content'])
-                preview_obj['og_image'] = meta.attrs['content']
-            if meta.attrs['property'] == 'og:description':
+            if 'image' in meta.attrs['property']:
+                url_obj['image'].add(meta.attrs['content'])
+            if 'description' in meta.attrs['property']:
                 url_obj['description'] = meta.attrs['content']
-                preview_obj['og_description'] = meta.attrs['content']
                 tags.append(meta.attrs['content'])
-            if meta.attrs['property'] == 'og:site_name':
+            if 'site_name' in meta.attrs['property']:
                 url_obj['site_name'] = meta.attrs['content']
-                preview_obj['og_site_name'] = meta.attrs['content']
                 tags.append(meta.attrs['content'])
-            if meta.attrs['property'] == 'og:type':
-                url_obj['item_type'] = meta.attrs['content']
-
     for tag_set in tags:
         tag_up = tag_set.lstrip(' ').rstrip(' ').replace(' ', '_')
         for tag in master_tags:
@@ -130,27 +90,100 @@ def build_url_obj(url):
                 url_obj['item_tags'][tag] += 1
             elif tag in tag_up and tag not in url_obj['item_tags']:
                 url_obj['item_tags'][tag] = 1
-    url_obj['preview_obj'] = preview_obj
     return url_obj
 
 
+def populate_master_tags():
+    global gmail_tags_path, master_tags
+    f_in = open(_path, 'r')
+    for line in f_in:
+        x = line.replace('\r\n', '').replace('\r', '').replace('\n', '')
+        y = x.lstrip(' ').rstrip(' ').replace(' ', '_')
+        master_tags.add(y.lower())
+
+
+def print_elapsed_time(_start_time):
+    seconds = round(int(time.time() - _start_time), 2)
+    minutes = math.trunc(seconds / 60)
+    remaining_seconds = math.trunc(seconds - (minutes * 60))
+    if len(f'{remaining_seconds}') != 2:
+        remaining_seconds = f'0{remaining_seconds}'
+    elapsed_time = f'{minutes}:{remaining_seconds}'
+    time_message = f'**** Total_Time Elapsed: {elapsed_time} =======================\n\n'
+    logging.info(time_message)
+
+
+def gmail_authorize(gmail_service):
+    try:
+        _return_value, _data = gmail_service.login(gmail_email, gmail_passwd)
+    except imaplib.IMAP4.error:
+        logging.warning('[!] Login Failed\n')
+        sys.exit(1)
+
+
+def process_mailbox(_gmail_service):
+    global url_blacklist, sender_whitelist, master_dict
+    _return_value, _data = _gmail_service.search(None, "ALL")
+    if _return_value != 'OK':
+        logging.warning('[!] No messages found!\n')
+        return
+    for num in _data[0].split():
+        _return_value, _data = _gmail_service.fetch(num, '(RFC822)')
+        if _return_value != 'OK':
+            logging.warning(f'[!] Error getting message {num}\n')
+            return
+        for response_part in _data:
+            if isinstance(response_part, tuple):
+                _message = email.message_from_string(response_part[1].decode('utf-8'))
+                email_from = _message['from']
+                for part in _message.walk():
+                    if part.get_content_type() == 'text/plain':
+                        message_payload = part.get_payload(decode=True).decode('utf-8')
+                        url_list = re.findall(r'(https?://\S+)', message_payload)
+                        for url in url_list:
+                            is_blacklisted = 0
+                            sender_approved = 0
+                            for ignore_url in url_blacklist:
+                                if ignore_url in url:
+                                    is_blacklisted = 1
+                            for sender in sender_whitelist:
+                                if sender in email_from:
+                                    sender_approved = 1
+                            if url not in master_dict and url not in url_blacklist:
+                                if is_blacklisted == 0 and sender_approved == 1:
+                                    url_obj = build_url_obj(url)
+                                    master_dict[url] = url_obj
+
+
+def parse_email_list(gmail_service):
+    global gmail_folder
+    return_value, data = gmail_service.select(gmail_folder)
+    if return_value == 'OK':
+        logging.info(f'[i] Processing mailbox: {gmail_folder}\n')
+        process_mailbox(gmail_service)
+        gmail_service.close()
+    else:
+        logging.warning(f'[!] Error: Unable to open mailbox `{return_value}`')
+
+
+def summarize_url_content(_url):
+    global simple_summarizer
+    html = urlopen(_url).read()
+    raw = nltk.clean_html(html)
+    summary = simple_summarizer.summarize(raw, 4)
+    return summary
+
+
 def main():
-    global _trash_set, master_dict
-    populate_master_tags()
-    url_list = gather_urls()
-    print(url_list)
-    for url in url_list:
-        url_object = build_url_obj(url)
-        master_dict[url] = url_object
-        # print(f'[URL] {url}')
-        # print('\n----------------------------\n')
-        # print(url_object)
-        # print('\n-------------------\n')
-        # print(_trash_set)
-    # for item in master_dict:
-    #    print(f'[URL] {item}', '\n--------------------\n')
-    #    print(f'[+] {master_dict[item]}', '\n')
+    global start_time, gmail_folder, master_dict
+    gmail_service = imaplib.IMAP4_SSL('imap.gmail.com')
+    gmail_authorize(gmail_service)
+    parse_email_list(gmail_service)
+    gmail_service.logout()
+    for url in master_dict:
+        print(f'[+] URL: {url}\n{master_dict[url]}')
+    print_elapsed_time(start_time)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
